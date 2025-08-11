69a6344c29f86186085cb5d760b816cae23665e9
pedro henrique <pedrohenriqueferreira9898@gmail.com>
2025-08-11 14:17:35 -0300

    Cabeçalho
    Iniciando do cabeçalho

---
+++ TCC Ivan Gabriel/home.php
@@ -0,0 +1,21 @@
+<?php
+session_start();
+
+
+$conectar = mysql_connect('localhost', username: 'root', password: '');
+if (!$conectar) {
+    die('Erro de conexão: ' . mysql_error());
+}
+$db = mysql_select_db(database_name: 'marcenaria', link_identifier: $conectar);
+if (!$db) {
+    die('Erro ao selecionar banco: ' . mysql_error());
+}
+
+
+
+
+
+
+            
+            
+ 
