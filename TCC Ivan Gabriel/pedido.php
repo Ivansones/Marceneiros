b9d1d8be24111b2456ee9fa122d9ca1a256d3624
Ivan Perera <ivangabrielcesa2@gmail.com>
2025-09-08 20:28:31 +0000

    Update pedido.php
    Arrumei os pedidos e ele lembrar do id do user

--- TCC Ivan Gabriel/pedido.php
+++ TCC Ivan Gabriel/pedido.php
@@ -1,7 +1,12 @@
 <?php
+session_start();
 $connect = mysql_connect('localhost','root','');
 $db = mysql_select_db('marcenaria');
 
+if (!$connect) {
+    die("Erro de conexão: " . mysqli_connect_error());
+}
+$user_id = $_SESSION['user_id'];
 
 if (isset($_POST['enviar'])) {
     $cpf = $_POST['cpf'];
@@ -16,21 +21,37 @@ if (isset($_POST['enviar'])) {
     $hora = $_POST['hora'];
     $descricao = $_POST['descricao'];
     $qual = $_POST['qual'];
+    $price = 0;
     
-    $prices = array(
-        'mesa'    => 600,
-        'cadeira' => 70,
-        'armario' => 700,
-        'tabua'   => 100
-    );
+    //Para o item base//
+    $sql = "SELECT preco FROM opcoes
+            WHERE nome = '$qual'";
+    $resposta = mysql_query($sql);
+    if ($resposta){
+    $dados = mysql_fetch_assoc($resposta);
+    if ($dados){    
+        $price = $dados['preco'];
 
-
-    if (isset($prices[$qual])){
-        $price = $prices[$qual];
+        }       
     }
 
-    $sql = "insert into pedidos(cpf,nome,email,telefone_celular,cep,cidade,endereco,bairro,data,hora,descricao,preco)
-    values('$cpf','$nome','$email','$telefone_celular','$cep','$cidade','$endereco','$bairro','$data','$hora','$descricao','$price')";
+    //Aqui é para os adicionais//
+    if (!empty($_POST['adicionas'])){
+        foreach($_POST['adicionas'] as $adicional) {
+        $sqladicional = "SELECT preco FROM adicionais
+                     WHERE nome = '$adicional'";
+        $respostaadi = mysql_query($sqladicional);
+        if ($respostaadi){
+        $dadosadi = mysql_fetch_assoc($respostaadi);
+        if ($dadosadi){    
+            $price = $price + $dadosadi['preco'];
+    
+            }       
+        }
+    }
+    }
+    $sql = "insert into pedidos(cpf,user_id,nome,email,telefone_celular,cep,cidade,endereco,bairro,data,hora,descricao,preco)
+    values('$cpf','$user_id','$nome','$email','$telefone_celular','$cep','$cidade','$endereco','$bairro','$data','$hora','$descricao','$price')";
 
 
     $resultado = mysql_query($sql);
