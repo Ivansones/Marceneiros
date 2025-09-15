d041b17fad3009358d71753ed28f324a0f8b6e9a
pedro henrique <pedrohenriqueferreira9898@gmail.com>
2025-09-15 13:29:44 -0300

    ola
    ola

--- TCC Ivan Gabriel/login.php
+++ TCC Ivan Gabriel/login.php
@@ -1,24 +1,17 @@
+
 <?php
-session_start();
 $connect = mysql_connect('localhost','root','');
 $db = mysql_select_db('marcenaria');
-
-
-if (!$connect) {
-    die("Erro de conexão: " . mysqli_connect_error());
-}
 
 if (isset($_POST['verificar'])){
 
     $nome = $_POST['nome'];
     $senha = $_POST['senha'];
 
-    $sql = "select id,nome,senha,tipo from usuario
+    $sql = "select nome,senha from usuario
             where senha ='$senha' and nome = '$nome'";
 
     $resultado = mysql_query($sql);
-
-    $dados = mysql_fetch_assoc($resultado);
 
     if (mysql_num_rows($resultado) <=0)
     {
@@ -29,8 +22,6 @@ if (isset($_POST['verificar'])){
     }
     else 
     {
-        $_SESSION['user_id'] = $dados['id'];
-        $_SESSION['user_name'] = $dados['nome'];
         setcookie('nome',$nome);
         header('location:home.html');
     }
@@ -38,5 +29,3 @@ if (isset($_POST['verificar'])){
 
 
 
-
-?>
