f563669c4342a3d706daccd75106c27ff1bd1865
Ivan Perera <ivangabrielcesa2@gmail.com>
2025-09-15 17:09:31 -0300

    Update login.php
    Agora ele lembra quase todas as informaçoes do usuario

--- TCC Ivan Gabriel/login.php
+++ TCC Ivan Gabriel/login.php
@@ -1,17 +1,24 @@
-
 <?php
+session_start();
 $connect = mysql_connect('localhost','root','');
 $db = mysql_select_db('marcenaria');
+
+
+if (!$connect) {
+    die("Erro de conexão: " . mysql_connect());
+}
 
 if (isset($_POST['verificar'])){
 
     $nome = $_POST['nome'];
     $senha = $_POST['senha'];
 
-    $sql = "select nome,senha from usuario
+    $sql = "select id,nome,senha,cpf,email,cep,cidade,telefone,endereco,bairro,tipo from usuario
             where senha ='$senha' and nome = '$nome'";
 
     $resultado = mysql_query($sql);
+
+    $dados = mysql_fetch_assoc($resultado);
 
     if (mysql_num_rows($resultado) <=0)
     {
@@ -22,6 +29,16 @@ if (isset($_POST['verificar'])){
     }
     else 
     {
+        $_SESSION['user_id'] = $dados['id'];
+        $_SESSION['user_nome'] = $dados['nome'];
+        $_SESSION['user_cpf'] = $dados['cpf'];
+        $_SESSION['user_email'] = $dados['email'];
+        $_SESSION['user_cep'] = $dados['cep'];
+        $_SESSION['user_cidade'] = $dados['cidade'];
+        $_SESSION['user_telefone'] = $dados['telefone'];
+        $_SESSION['user_endereco'] = $dados['endereco'];
+        $_SESSION['user_bairro'] = $dados['bairro'];
+        $_SESSION['user_tipo'] = $dados['tipo'];
         setcookie('nome',$nome);
         header('location:home.html');
     }
@@ -29,3 +46,5 @@ if (isset($_POST['verificar'])){
 
 
 
+
+?>
