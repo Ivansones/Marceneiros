062cdae0ced5ae62aa5ffb7a1409381fe95ec321
Ivan Perera <ivangabrielcesa2@gmail.com>
2025-11-03 13:18:59 -0300

    Arrumando a funcionalidade
    Agora o cart realiza a venda salvando o id do usuario e data da comprar , tambem arrumei para diminuir a quantidade e chechar se ta zerado

--- TCC Ivan Gabriel/cart.php
+++ TCC Ivan Gabriel/cart.php
@@ -2,6 +2,9 @@
 $connect = mysql_connect('localhost','root','');
 $db = mysql_select_db('marcenaria');
 session_start();
+
+$user_id = $_SESSION['user_id'];
+$user_tipo = $_SESSION['user_tipo'];
 $status="";
 if (isset($_POST['action']) && $_POST['action']=="remove")
 {
@@ -19,14 +22,61 @@ if (isset($_POST['action']) && $_POST['action']=="remove")
 }
 
 if (isset($_POST["comprar"])){
-    $sql = "insert into vendas (id,nome,descricao,preco,imagen) 
-            values ("
+    $fora_de_estoque = array();
+
+    foreach ($_SESSION["shopping_cart"] as $key => $product){
+        
+        $productid = $product['id'];
+
+        $sqlquant = "select quantidade from itens where id = '".$productid."'";
+
+        $result = mysql_query($sqlquant);
+
+        $row = mysql_fetch_assoc($result);
+
+        if ($row) {
+            $currentQuantity = $row['quantidade'];
+            $data_hora = date("Y-m-d H:i:s");
+            if ($currentQuantity == 0){
+                $fora_de_estoque[] = $product['nome'];
+                continue;
+            }
+            else{
+            $sqladiciona = "Insert into vendas(nome,descricao,preco,id_usuario,data,imagen)
+                    values('".$product['nome']."','".$product['descricao']."','".$product['preco']."','".$user_id."','".$data_hora."','".$product['imagen']."')";
+       
+                
+            $resultado = mysql_query($sqladiciona);
+            if ($resultado == TRUE){
+               $quantidadefinal = $currentQuantity - 1;
+               $sqlarruma = "UPDATE itens SET quantidade = '$quantidadefinal' WHERE id = '$productid'";
+               mysql_query($sqlarruma);
+            } else{
+                echo "deu errado";
+            }
+            }
+        }
+        if (!empty($fora_de_estoque)) {
+        $nomes = implode(", ", $fora_de_estoque);
+        echo "<script>
+                alert('Compra concluída! Porém, os seguintes produtos estavam fora de estoque: $nomes');
+                window.location.href = 'home.php';
+              </script>";
+        }else {
+        echo "<script>
+                alert('Compra realizada com sucesso!');
+                window.location.href = 'home.php';
+              </script>";
+    }
+    }
+    unset($_SESSION['shopping_cart']);
 }
 ?>
 <HTML>
 <HEAD>
  <TITLE>Carrinho Compras </TITLE>
   <link rel="stylesheet" href="estilo.css">
+  <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 </HEAD>
 <BODY>
 
@@ -83,8 +133,8 @@ if(isset($_SESSION["shopping_cart"])){
 <div class="message_box" style="margin:10px 0px;">
 <?php echo $status; ?>
 </div>
-<form action="cart.php" method="submit">
-    <input type="submit" name="comprar" id="comprar">;
+<form action="cart.php" method="post">
+    <input type="submit" name="comprar" id="comprar" placeholder="Comprar">
 </form>
 
 </BODY>
